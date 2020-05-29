66bd0798cc5ce33169c14f464a1171cded632f66
invertko <jonghyun4501@naver.com>
2020-05-29 17:09:59 +0900

    'start_20200529'

---
+++ src/components/menu/menulist.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-list class="db-menu-list">
+    <menu-list-item
+      v-for="item in menuItems"
+      v-bind:key="item.id"
+      :item="item"
+      :collapsed="collapsed"
+      :active-class="activeClass"
+      @expansionItemClick="onExpansionItemClick(item)"
+      @itemExpanded="onItemExpanded(item)"
+      @itemCollapsed="onItemCollapsed(item)"
+    />
+  </q-list>
+</template>
+<script>
+import MenuListItem from './menulistitem.vue';
+export default {
+  name: 'MenuList',
+  components: { MenuListItem },
+  props: {
+    menuItems: {
+      type: Array,
+      default: null
+    },
+    collapsed: {
+      type: Boolean,
+      default: false
+    },
+    activeClass: {
+      type: String,
+      default: ''
+    }
+  },
+  data: function() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    onExpansionItemClick(item) {
+      this.$emit('expansionItemClick', item);
+    },
+    onItemExpanded(item) {
+      this.$emit('itemExpanded', item);
+    },
+    onItemCollapsed(item) {
+      this.$emit('itemCollapsed', item);
+    }
+  }
+};
+</script>
+<style lang="scss">
+.db-menu-list {
+  a,
+  a:hover {
+    text-decoration: none;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,86 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/store';
+import VueGoodTablePlugin from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css';
 
-Vue.config.productionTip = false
+import {
+  DashBlocks,
+  DbDashboard,
+  DbHorizon,
+  DbSunburst,
+  DbSankey,
+  DbRidgeline,
+  DbChartjsBar,
+  DbChartjsHorizontalBar,
+  DbChartjsDoughnut,
+  DbChartjsLine,
+  DbChartjsPie,
+  DbChartjsPolarArea,
+  DbChartjsRadar,
+  DbChartjsBubble,
+  DbChartjsScatter,
+  DbNumber,
+  DbEasyPie,
+  DbTrendLine,
+  DbTrendBar,
+  DbSparkline,
+  DbDygraphsBar,
+  DbDygraphsLine,
+  DbDygraphsSparkLine,
+  DbDygraphsDateTimeHistogram
+} from 'dashblocks';
+
+Vue.use(DashBlocks, {
+  components: {
+    DbDashboard,
+    DbHorizon,
+    DbSunburst,
+    DbSankey,
+    DbRidgeline,
+    DbChartjsBar,
+    DbChartjsHorizontalBar,
+    DbChartjsDoughnut,
+    DbChartjsLine,
+    DbChartjsPie,
+    DbChartjsPolarArea,
+    DbChartjsRadar,
+    DbChartjsBubble,
+    DbChartjsScatter,
+    DbNumber,
+    DbEasyPie,
+    DbTrendLine,
+    DbTrendBar,
+    DbSparkline,
+    DbDygraphsBar,
+    DbDygraphsLine,
+    DbDygraphsSparkLine,
+    DbDygraphsDateTimeHistogram
+  }
+  //components: dashblocksComponents
+});
+
+// Sample: to import all
+/*
+import { DashBlocks } from 'dashblocks';
+import * as dashblocksComponents from 'dashblocks';
+Vue.use(DashBlocks, {
+  components: dashblocksComponents
+});
+*/
+
+// Dashblocks CSS
+import 'dashblocks/dist/dashblocks.css';
+
+import './quasar';
+
+//Vue.config.productionTip = false;
+
+Vue.use(VueGoodTablePlugin);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
